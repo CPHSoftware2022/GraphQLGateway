c8c6de0ada498a362d4aaf33b70ad8beded0494e
Matti <56347553+MattiBSH@users.noreply.github.com>
2022-12-09 14:44:13 +0100

    First working version

---
+++ .mesh/sources/courier_gateway/schema.graphql.js
@@ -0,0 +1,38 @@
+const { buildSchema, Source } = require('graphql');
+
+const source = new Source(/* GraphQL */`
+schema {
+  query: Query
+  mutation: Mutation
+});
+
+type Query{
+  # Fetch All Couriers
+  findAllCouriers: [Courier]
+  findOneCourier(id: ID): [Courier]
+  findAvailableCouriers(available: Boolean): [Courier]
+
+}
+
+type Mutation {
+#    # Update the courier status
+  addCourier(firstName: String, lastName: String, email: String, password: String, available: Boolean): Courier
+  update(id: ID, firstName: String, lastName: String, email: String, password: String, available: Boolean): Courier
+  delete(id: ID): Courier
+}
+
+type Courier {
+  courierId: ID!
+  firstName: String!
+  lastName: String!
+  email: String!
+  password: String!
+  available: Boolean!
+}
+
+`, `.mesh/sources/courier_gateway/schema.graphql`);
+
+module.exports = buildSchema(source, {
+  assumeValid: true,
+  assumeValidSDL: true
+});
